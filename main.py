61340e8a1b0f961ccbc19fca0154b8924e0a3325
sialabit <sialabit@gmail.com>
2019-11-02 16:02:33 +0900

    enable to get number of accesses per an hour

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import getopt
 import glob
+from datetime import datetime, date, time
 import hashlib
 from pprint import pprint
 
@@ -17,8 +18,6 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-data = []
-
 def cmd_handler():
     # if not len(sys.argv[1:]):
     #     print('Usage')
@@ -30,20 +29,37 @@ def cmd_handler():
                 'm:',
                 ['mode='])
     except getopt.GetoptError as err:
-        print str(err)
+        print(str(err))
 
 def line_parser(line):
     parser = apache_log_parser.make_parser('%h %l %u %t "%r" %>s %b "%{Referer}i" "%{User-Agent}i"')
-    # pprint(parser(line))
     return parser(line)
 
+def resided_ts(dt):
+    resided_dt = dt.replace(minute=0, second=0, microsecond=0) 
+    return resided_dt.timestamp()
+
+data = []
 
 def main():
     cmd_handler()
 
+    each_hour = {}
+
     for fname in glob.glob(os.path.join(LOG_DIR, '*.log')):
         with open(fname) as f:
             for l in f:
-                data.append(line_parser(l))
+                # data.append(line_parser(l))
+                parsed = line_parser(l)
+                ts = resided_ts(parsed['time_received_tz_datetimeobj'])
+
+                each_hour[ts] = each_hour.get(ts, 0) + 1
+    
+    for ts in each_hour:
+        num_ts = float(ts)
+        dt = datetime.fromtimestamp(num_ts)
+
+        print(dt.strftime('%d/%b/%Y:%H:%M'), each_hour[ts])
+
 
 main()
